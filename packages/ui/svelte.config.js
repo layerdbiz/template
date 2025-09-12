cb313f9c2a459d154cf3e438cc10761734bcee6b
oneezy <justinoneill2007@gmail.com>
2025-09-12 15:35:54 -0500

    maybe fixed?

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -1,5 +1,5 @@
 import { resolve } from "path";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
